9fb7158e9dbbe642d0f6fea60d1b88e797f0bc5e
Hao Yang <hlee0163@student.monash.edu>
2025-10-18 00:28:29 +0800

    added violin chart

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,12 @@
-var vg_3 = "./vega-lite/stations.vg.json"
-var vg_4 = "./vega-lite/area_chart.vg.json"
-var vg_5 = "./vega-lite/aqi_states.vg.json"
+var choropleth = "./vega-lite/stations.vg.json"
+var area_chart = "./vega-lite/area_chart.vg.json"
+var bar_chart = "./vega-lite/aqi_states.vg.json"
 
 // store views globally
 let views = {};
 
-vegaEmbed("#stations", vg_3, {"actions": false}).then(function (result) {
+// first section
+vegaEmbed("#stations", choropleth, {"actions": false}).then(function (result) {
     views.stations = result.view;
     const slider = document.getElementById('timeSlider');
     const label = document.getElementById('timeLabel');
@@ -28,12 +29,12 @@ vegaEmbed("#stations", vg_3, {"actions": false}).then(function (result) {
 
 }).catch(console.error);
 
-vegaEmbed("#aqi_monthly", vg_4, {"actions": false}).then(function (result) {
+vegaEmbed("#aqi_dist", area_chart, {"actions": false}).then(function (result) {
     views.aqi_monthly = result.view;
 
 }).catch(console.error);
 
-vegaEmbed('#aqi_states', vg_5, {"actions": false}).then(function (result) {
+vegaEmbed('#aqi_states', bar_chart, {"actions": false}).then(function (result) {
     views.aqi_states = result.view;
 
     // listen to state selection changes
@@ -52,6 +53,14 @@ vegaEmbed('#aqi_states', vg_5, {"actions": false}).then(function (result) {
         // update area chart to focus on state
         views.aqi_monthly.signal('selected_state', selectedState).run();
     });
+
+}).catch(console.error);
+
+var violin = "./vega-lite/violin_chart.vg.json"
+
+// second section
+vegaEmbed("#aqi_dist", violin, {"actions": false}).then(function (result) {
+    views.test = result.view;
 
 }).catch(console.error);
 
